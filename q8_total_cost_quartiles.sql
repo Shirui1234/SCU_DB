17c550aff834c9181e15a8f120c5101ed6af2cd9
Shirui1234 <72693913+Shirui1234@users.noreply.github.com>
2021-10-04 20:38:46 +0800

    Delete q8_total_cost_quartiles.sql

--- q8_total_cost_quartiles.sql
+++
@@ -1,9 +0,0 @@
-with Expenditures as (
-select IFNULL(CompanyName,'MISSING_NAME') as CompanyName,CustomerId,Round(sum(Quantity*UnitPrice),2) as TotalExpend
-from 'Order' join OrderDetail on 'Order'.Id=OrderId left join Customer on Customer.Id=CustomerId
-group by CustomerId),
-Quartiles as (select *,NTILE(4) over (order by totalExpend) as Quartile from Expenditures)
-select CompanyName,CustomerId,TotalExpend
-from Quartiles
-where Quartile=1
-order by TotalExpend;
