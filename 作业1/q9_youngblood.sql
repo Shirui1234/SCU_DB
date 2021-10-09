2ae7b03796f92b3ec1a755442c90a766f06485a2
Shirui1234 <72693913+Shirui1234@users.noreply.github.com>
2021-10-09 14:28:39 +0800

    Delete q9_youngblood.sql

--- 作业1/q9_youngblood.sql
+++
@@ -1,5 +0,0 @@
-select RegionDescription,FirstName, LastName,max(BirthDate)
-from Employee,EmployeeTerritory,Territory,Region
-where Employee.Id=EmployeeId and TerritoryId=Territory.Id and RegionId=Region.Id
-group by Region.Id
-order by Region.Id;
