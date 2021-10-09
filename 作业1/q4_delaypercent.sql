51caa53ace499dc569616f39c87bdd487a433c8c
Shirui1234 <72693913+Shirui1234@users.noreply.github.com>
2021-10-09 14:27:22 +0800

    Delete q4_delaypercent.sql

--- 作业1/q4_delaypercent.sql
+++
@@ -1,3 +0,0 @@
-select CompanyName,Round(100.*(select count(*) from 'Order' where Shipper.Id='Order'.ShipVia and ShippedDate>RequiredDate)/(select count(*) from 'Order' where Shipper.Id='Order'.ShipVia),2) as delaypercent
-from Shipper
-order by delaypercent desc;
