9b58f91f006191cdfb5e65ed2ba00915b175ab07
Shirui1234 <72693913+Shirui1234@users.noreply.github.com>
2021-10-09 14:27:38 +0800

    Delete q5_aggregates.sql

--- 作业1/q5_aggregates.sql
+++
@@ -1,6 +0,0 @@
-select CategoryName,count(*),Round(avg(UnitPrice),2),min(UnitPrice),max(UnitPrice),sum(UnitsOnOrder)
-from Product,Category
-where CategoryId=Category.Id
-group by CategoryId
-having count(*)>10
-order by CategoryId;
